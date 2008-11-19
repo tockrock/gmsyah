7bdfcdd0a7b82884378489bd72b7152d7ed254b7
Takeshi Suzuki <tockrock@gmail.com>
2008-11-19 14:29:25 +0900

    initial commit

---
+++ gmsYAH.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name          GMS Yahoo! Japan Auction Helper
+// @namespace     http://www26.atwiki.jp/mtg_gms/
+// @description   Helps the use of Yahoo! Auction for GMS.
+// @include       http://*auctions.yahoo.co.jp/jp/show/submit
+// @require	  http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.js       
+// ==/UserScript==
+
+
+
